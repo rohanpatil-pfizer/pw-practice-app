70bd12cc6c08562a6cdc12f4e795f9603722273f
Rohan Patil <rohan.patil@pfizer.com>
2025-03-10 18:01:18 +0000

    Common code added to beforeeach

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -2,20 +2,17 @@ import { test } from "@playwright/test";
 
 
 test.beforeEach(async ({ page }) => {
-
+  await page.goto("localhost:4200");
+  await page.getByText('Forms').click();
 });
 
 test.describe("first test suite", () => {
   
   test("Navigate to Forms", async ({ page }) => {
-    await page.goto("localhost:4200");
-    await page.getByText('Forms').click();
     await page.getByText('Form Layouts').click();
   });
 
-  test("Navigate to DatePicker", async({page}) => {
-    await page.goto("localhost:4200");
-    await page.getByText('Forms').click();
+  test("Navigate to DatePicker", async({page}) => {  
     await page.getByText('Datepicker').click();    
   });
 });
