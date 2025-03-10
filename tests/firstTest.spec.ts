7a9b6e4ce6b3e243219e363db6e98a9bb5a29aef
Rohan Patil <rohan.patil@pfizer.com>
2025-03-10 18:16:38 +0000

    code updated get by Role to match exact words

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,18 +1,29 @@
 import { test } from "@playwright/test";
 
-
 test.beforeEach(async ({ page }) => {
   await page.goto("localhost:4200");
-  await page.getByText('Forms').click();
 });
 
 test.describe("first test suite", () => {
-  
-  test("Navigate to Forms", async ({ page }) => {
-    await page.getByText('Form Layouts').click();
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole('link', { name: 'Forms' }).click();
   });
 
-  test("Navigate to DatePicker", async({page}) => {  
-    await page.getByText('Datepicker').click();    
+  test("Navigate to Forms", async ({ page }) => {
+    await page.getByRole('link', { name: 'Form Layouts' }).click();
+  });
+
+  test("Navigate to DatePicker", async ({ page }) => {
+    await page.getByRole('link', { name: 'Datepicker' }).click();
+  });
+});
+
+test.describe("second test suite", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole('link', { name: 'Charts', exact: true }).click();
+  });
+
+  test("Navigate to Charts", async ({ page }) => {
+    await page.getByRole('link', { name: 'Echarts' }).click();
   });
 });
