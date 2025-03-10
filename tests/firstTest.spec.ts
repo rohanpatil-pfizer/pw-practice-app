40dbef9b45181e0559049683be5d1b6ace12ad1c
Rohan Patil <rohan.patil@pfizer.com>
2025-03-10 18:00:48 +0000

    2 tests added

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,10 +1,21 @@
 import { test } from "@playwright/test";
 
+
+test.beforeEach(async ({ page }) => {
+
+});
+
 test.describe("first test suite", () => {
+  
   test("Navigate to Forms", async ({ page }) => {
     await page.goto("localhost:4200");
     await page.getByText('Forms').click();
     await page.getByText('Form Layouts').click();
   });
-  test("second test", () => {});
+
+  test("Navigate to DatePicker", async({page}) => {
+    await page.goto("localhost:4200");
+    await page.getByText('Forms').click();
+    await page.getByText('Datepicker').click();    
+  });
 });
