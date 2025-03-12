9fd8970f4290d163ead828618fd45fb05f8ed6c8
Rohan Patil <rohan.patil@pfizer.com>
2025-03-12 16:21:49 +0000

    Auto Waiting 2

--- tests/autowaiting.spec.ts
+++ tests/autowaiting.spec.ts
@@ -8,13 +8,14 @@ test.beforeEach(async ({page}) => {
 
 test('Auto Waiting', async ({page}) => {
   const successtext = page.locator('.bg-success');
-  
   // await successtext.click();
-  
-  
   //const text1 = await successtext.textContent(); // this will wait for the text to appear even if the default timeout is modified in config 
-  
-  const text1 = await successtext.allTextContents(); // this will not wait 
-  expect(text1).toBe('Data loaded with Ajax get request.');
+   
+  await successtext.waitFor({state: 'attached'}); // this will wait for the element to be visible needed for the method in next line to wait
+  const text1 = await successtext.allTextContents(); 
+  expect(text1).toContain('Data loaded with AJAX get request.');
+
+  // expect has a timeout of 5 seconds, hence if we comment the above 3 lines then this below assertion will fail
+  await expect(successtext).toHaveText('Data loaded with AJAX get request.'); 
 
 });
