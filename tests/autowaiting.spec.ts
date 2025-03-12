4c6dd96136f72fc1aa557ad6b94b263c1e4a8e47
Rohan Patil <rohan.patil@pfizer.com>
2025-03-12 16:23:23 +0000

    autowaiting 5 seconds assertion failure

--- tests/autowaiting.spec.ts
+++ tests/autowaiting.spec.ts
@@ -11,9 +11,9 @@ test('Auto Waiting', async ({page}) => {
   // await successtext.click();
   //const text1 = await successtext.textContent(); // this will wait for the text to appear even if the default timeout is modified in config 
    
-  await successtext.waitFor({state: 'attached'}); // this will wait for the element to be visible needed for the method in next line to wait
-  const text1 = await successtext.allTextContents(); 
-  expect(text1).toContain('Data loaded with AJAX get request.');
+  // await successtext.waitFor({state: 'attached'}); // this will wait for the element to be visible needed for the method in next line to wait
+  // const text1 = await successtext.allTextContents(); 
+  // expect(text1).toContain('Data loaded with AJAX get request.');
 
   // expect has a timeout of 5 seconds, hence if we comment the above 3 lines then this below assertion will fail
   await expect(successtext).toHaveText('Data loaded with AJAX get request.'); 
