86ef5f41dea52f12df4fbca12cf049938a39e064
Rohan Patil <rohan.patil@pfizer.com>
2025-03-12 14:30:13 +0000

    locating child elements

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -10,7 +10,7 @@ test.describe("first test suite", () => {
     await page.getByRole("link", { name: "Form Layouts" }).click();
   });
 
-  test("Locator syntax Rules", async ({ page }) => {
+  test.skip("Locator syntax Rules", async ({ page }) => {
     //By Tag name
     page.locator("input").first().click();
 
@@ -40,7 +40,7 @@ test.describe("first test suite", () => {
   });
 
   // https://playwright.dev/docs/api/class-framelocator#frame-locator-frame-locator
-  test("User facing locators", async ({ page }) => {
+  test.skip("User facing locators", async ({ page }) => {
     // await page.getByRole('textbox', { name: 'Email' }).first().click();
     // await page.getByRole('button', { name: 'Sign in' }).first().click();
 
@@ -48,6 +48,16 @@ test.describe("first test suite", () => {
     // await page.getByPlaceholder('Jane Doe').click();
     await page.getByTestId("SignIn").click();
   });
+
+  test('Locating Child elements',async ({page})=>{  
+     await page.locator('nb-card nb-radio :text-is("Option 2")').click();
+      //await page.locator('nb-card').getByRole('button',{name:'Sign in'}).click();
+     await page.locator('nb-card').getByRole('button',{name:'Sign in'}).click(); 
+  });
+
+  
+
+
 });
 
 test.describe.skip("second test suite", () => {
