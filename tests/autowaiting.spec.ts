9a3cc4a824a54c422ffcdcdffd17a577a8713cc6
Rohan Patil <rohan.patil@pfizer.com>
2025-03-12 16:32:33 +0000

    Autowaiting 4 Wait for Element  selector

--- tests/autowaiting.spec.ts
+++ tests/autowaiting.spec.ts
@@ -11,11 +11,23 @@ test('Auto Waiting', async ({page}) => {
   // await successtext.click();
   //const text1 = await successtext.textContent(); // this will wait for the text to appear even if the default timeout is modified in config 
    
-  // await successtext.waitFor({state: 'attached'}); // this will wait for the element to be visible needed for the method in next line to wait
-  // const text1 = await successtext.allTextContents(); 
-  // expect(text1).toContain('Data loaded with AJAX get request.');
+  await successtext.waitFor({state: 'attached'}); // this will wait for the element to be visible needed for the method in next line to wait
+  const text1 = await successtext.allTextContents(); 
+  expect(text1).toContain('Data loaded with AJAX get request.');
 
   // expect has a timeout of 5 seconds, hence if we comment the above 3 lines then this below assertion will fail
   await expect(successtext).toHaveText('Data loaded with AJAX get request.',{timeout:20000}); 
+
+});
+
+
+test('Alternative Waits', async ({page}) => {
+  const successBtn = page.locator('.bg-success');
+
+  //  wait for element 
+   await page.waitForSelector('.bg-success');
+
+  const text = await successBtn.allTextContents(); 
+  expect(text).toContain('Data loaded with AJAX get request.');
 
 });
